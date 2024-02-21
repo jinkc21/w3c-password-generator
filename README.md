202304661c24beb5b3dd2eb3a968b3a7f4c5f82b
Jinkc21 <jin.k.choe@gmail.com>
2024-02-21 16:07:20 -0500

    removed some unnecessary code and finalized readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # w3c-password-generator
+
+## Description
+
+This is a password generator that helps users create a random password based on user inputs. The user may decide the length of the password and whether or not it includes numbers, lowercase letters, uppercase letters, and/or special characters. If requirements are not met, the generator will restart.
+
+
+## Installation
+
+N/A
+
+## Usage
+
+To access the password generator, visit https://jinkc21.github.io/w3c-password-generator/
+
+
+The website should look like this sample image:
+
+![Sample Image](\assets\03-javascript-homework-demo.png)
+
+
+## Credits
+
+Several develor sites were used as resources to help write this program. Follow these links to learn more.
+
+Window Alert Methood: https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+Alerts: https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+Confirm: https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
+Prompt: https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+Math: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+GitHub was also used to store and publish the website.
+https://github.com/
+
+## License
+
+MIT License
+
+Copyright (c) 2024 jinkc21
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+---
--- script.js
+++ script.js
@@ -16,7 +16,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // Add generator code
 
-// variables for available arrays and choices.
+
 const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const allNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -24,83 +24,87 @@ const specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ","
 
 var choices;
 
-// running funtion
+
 function generatePassword() {
 
-  //  adding user input 
   var passwordLength = parseInt(prompt("How many characters for the password? Must choose between 8-128"));
 
-  // requires input between 8-128
   if (!passwordLength) {
     alert("This needs a value");
-  } else if (passwordLength < 8 || passwordLength > 128) {
-
-    // passwordLength = parseInt(prompt("You must choose between 8-128"));
+  } 
+  
+  else if (passwordLength < 8 || passwordLength > 128) {
     alert("Number must be between 8 and 128!");
     return;
+  } 
   
-    // confirms use of each option
-  } else {
+  else {
     var lowerCases = confirm("Will this password use lowercase letters?");
     var upperCases = confirm("Will this password use uppercase letters?");
     var numbers = confirm("Will this password use numbers?");
     var special = confirm("Will this password use special characters?");
-
   };
-  //  alerts if all confirms are false
+
   if (!special && !numbers && !upperCases && !lowerCases) {
     choices = alert("Conditions have not been met. Please try again.");
   }
-  // these else if statements collects user inputs.
 
-  // true to all four
   else if (special && numbers && upperCases && lowerCases) {
     choices = specialCharacters.concat(allNumbers, lowerLetters, upperLetters);
   }
 
-  // true to 3 conditions
   else if (special && numbers && upperCases) {
     choices = specialCharacters.concat(allNumbers, upperLetters);
   }
+
   else if (special && numbers && lowerCases) {
     choices = specialCharacters.concat(allNumbers, lowerLetters);
   }
+
   else if (special && lowerCases && upperCases) {
     choices = specialCharacters.concat(lowerLetters, upperLetters);
   }
+
   else if (numbers && lowerCases && upperCases) {
     choices = allNumbers.concat(lowerLetters, upperLetters);
   }
 
-  // true to 2 conditons
   else if (special && numbers) {
     choices = specialCharacters.concat(allNumbers);
-
-  } else if (special && lowerCases) {
+  } 
+  
+  else if (special && lowerCases) {
     choices = specialCharacters.concat(lowerLetters);
-
-  } else if (special && upperCases) {
+  } 
+  
+  else if (special && upperCases) {
     choices = specialCharacters.concat(upperLetters);
   }
+  
   else if (lowerCases && numbers) {
     choices = lowerLetters.concat(allNumbers);
-
-  } else if (lowerCases && upperCases) {
+  } 
+  
+  else if (lowerCases && upperCases) {
     choices = lowerLetters.concat(upperLetters);
-
-  } else if (numbers && upperCases) {
+  } 
+  
+  else if (numbers && upperCases) {
     choices = allNumbers.concat(upperLetters);
   }
-  // true to one condition
+
   else if (special) {
     choices = specialCharacters;
   }
+
   else if (numbers) {
     choices = allNumbers;
   }
+
   else if (lowerCases) {
     choices = lowerLetters;
   }
+
   else if (upperCases) {
     choices = upperLetters;
   };
